e55c289d4edd0c879c7343adfedfc18a20597dfd
Jester Longakit <jester@orientalwallet.com>
2023-05-11 17:27:13 +0800

    initial push

---
+++ jpayAPICypress/cypress/e2e/string_handler.js
@@ -0,0 +1,20 @@
+
+var based_url = {
+    dev: "https://api-develop.paystage.net/",
+    stg: "https://api-staging.paystage.net/",
+    prd: "https://prod-api-integrate.paystage.net/"
+}
+
+var api_pk = {
+    dev: "pk_FHmnhpf8iX8CeZd385tt8RwoqYguL42G",
+    stg: "pk_ox2x8O8b69ZqnYJApXsiTLhC7fBCwqhD",
+    prd: "pk_3RzlkRiMAVLTdqsvCZAmAblrVEd5IoEP"
+}
+
+var api_diggest = {
+    dev: "129455db9b759ede77fd1c2d89730478",
+    stg: "86c108420f765bd4f717885a4f581d54",
+    prd: "da7054554ea124e213f5ce659f97f810"
+}
+
+module.exports = {based_url, api_pk, api_diggest};
